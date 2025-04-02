9d17154b2ccaca74fc2c6bcc2233b1a92df1a60c
user2230 <furkanefeceylan2008@hotmail.com>
2025-04-02 13:01:17 +0200

    sunupdown

--- js/grafieken.js
+++ js/grafieken.js
@@ -18,7 +18,10 @@ setInterval(function(){
 
   !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
 
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
   function formatDate(date) {
     return date.toISOString().split('T')[0];
 }
@@ -77,7 +80,11 @@ fetch('zonnepanelen.json')
  
 main();
 
+<<<<<<< Updated upstream
 
+=======
+  
+>>>>>>> Stashed changes
 
 //   function setCookie(colorScheme, setScheme, exdays) {
 //     const d = new Date();
